a6b9ed73c4ac49bfb4a8f86fa7c2109de394e272
Alec Cowan <alec400@gmail.com>
2020-04-22 18:12:10 +0100

    Add Fody.PropertyChanged

--- PCSX2_Configurator.Frontend.Wpf/GameModel.cs
+++ PCSX2_Configurator.Frontend.Wpf/GameModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PropertyChanged;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -9,43 +10,17 @@ namespace PCSX2_Configurator.Frontend.Wpf
     {
         public string Game { get; set; }
         public string Path { get; set; }
-        public static IEnumerable<string> Versions { get; set; }
-        public IEnumerable<Tuple<string, bool>> VersionsAndStates => Versions.Select(version => new Tuple<string, bool>(version, Version == version));
-        private string version;
-        public string Version 
-        {
-            get => version;
-            set
-            {
-                version = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Version)));
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(VersionsAndStates)));
-            }
-        }
-        public static IEnumerable<string> Configs { get; set; }
-        public IEnumerable<Tuple<string, bool>> ConfigsAndStates => Configs.Select(config => new Tuple<string, bool>(config, Config == config));
-        private string config;
-        public string Config 
-        {
-            get => config;
-            set
-            {
-                config = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Config)));
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ConfigsAndStates)));
-            }
-        }
-        public string coverPath;
-        public string CoverPath 
-        {
-            get => coverPath; 
-            set
-            {
-                coverPath = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CoverPath)));
-            }
-        }
+        public string CoverPath { get; set; }
 
+        public static IEnumerable<string> Versions { get; set; }
+        public static IEnumerable<string> Configs { get; set; }
+
+        public IEnumerable<Tuple<string, bool>> VersionsAndStates => Versions.Select(version => new Tuple<string, bool>(version, Version == version));
+        public IEnumerable<Tuple<string, bool>> ConfigsAndStates => Configs.Select(config => new Tuple<string, bool>(config, Config == config));
+
+        [AlsoNotifyFor(nameof(VersionsAndStates))] public string Version { get; set; }
+        [AlsoNotifyFor(nameof(ConfigsAndStates))] public string Config { get; set; }
+        
         public event PropertyChangedEventHandler PropertyChanged;
     }
 }
